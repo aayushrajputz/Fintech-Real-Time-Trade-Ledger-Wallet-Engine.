6ea4a26d0f9324c8f7ae7f9fe467526d59cd5efb
aayushrajputz <aayushpratapsingh001@gmail.com>
2026-08-25 21:05:46 +0530

    implement BullMq

---
+++ src/config/queue.ts
@@ -0,0 +1,14 @@
+import { Queue } from "bullmq";
+import { env } from "./env.config.js";
+import { Query } from "pg";
+
+export const queueConnection = {
+    host: env.REDIS_HOST,
+    port: env.REDIS_PORT,
+    password: env.REDIS_PASSWORD || undefined,
+
+}
+
+export const orderQueue = new Queue("order-processing", {
+    connection: queueConnection
+}) 
